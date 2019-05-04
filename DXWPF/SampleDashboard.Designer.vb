fc0d4711779aa6beb7e89565a2de1147ee002e68
ncxn <ncxn@outlook.com>
2019-05-04 21:07:54 +0700

    Add project files.

---
+++ DXWPF/SampleDashboard.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SampleDashboard
+    Inherits DevExpress.DashboardCommon.Dashboard
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
+        Dim measure1 As New DevExpress.DashboardCommon.Measure()
+        Dim measure2 As New DevExpress.DashboardCommon.Measure()
+        Dim measure3 As New DevExpress.DashboardCommon.Measure()
+        Dim valueMap1 As New DevExpress.DashboardCommon.ValueMap()
+        Dim uniformScale1 As New DevExpress.DashboardCommon.UniformScale()
+        Dim deltaMap1 As New DevExpress.DashboardCommon.DeltaMap()
+        Dim dimension2 As New DevExpress.DashboardCommon.Dimension()
+        Dim gridDimensionColumn1 As New DevExpress.DashboardCommon.GridDimensionColumn()
+        Dim measure4 As New DevExpress.DashboardCommon.Measure()
+        Dim measure5 As New DevExpress.DashboardCommon.Measure()
+        Dim gridDeltaColumn1 As New DevExpress.DashboardCommon.GridDeltaColumn()
+        Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(SampleDashboard))
+        Dim fieldInfo1 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo2 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo3 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo4 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo5 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo6 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo7 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo8 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim fieldInfo9 As New DevExpress.DataAccess.Excel.FieldInfo()
+        Dim excelWorksheetSettings1 As New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
+        Dim excelSourceOptions1 As New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
+        Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
+        Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+        Dim dashboardLayoutItem2 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+        Me.choroplethMapDashboardItem1 = New DevExpress.DashboardCommon.ChoroplethMapDashboardItem()
+        Me.gridDashboardItem1 = New DevExpress.DashboardCommon.GridDashboardItem()
+        Me.dashboardExcelDataSource1 = New DevExpress.DashboardCommon.DashboardExcelDataSource()
+        CType(Me.choroplethMapDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(measure2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(measure3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(dimension2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(measure4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(measure5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        '
+        ' choroplethMapDashboardItem1
+        '
+        Me.choroplethMapDashboardItem1.Area = DevExpress.DashboardCommon.ShapefileArea.USA
+        dimension1.DataMember = "State"
+        Me.choroplethMapDashboardItem1.AttributeDimension = dimension1
+        Me.choroplethMapDashboardItem1.AttributeName = "NAME"
+        Me.choroplethMapDashboardItem1.ComponentName = "choroplethMapDashboardItem1"
+        measure1.DataMember = "RevenueYTD (Sum)"
+        measure2.DataMember = "RevenueYTD (Sum)"
+        measure3.DataMember = "RevenueYTDTarget (Sum)"
+        Me.choroplethMapDashboardItem1.DataItemRepository.Clear()
+        Me.choroplethMapDashboardItem1.DataItemRepository.Add(dimension1, "DataItem0")
+        Me.choroplethMapDashboardItem1.DataItemRepository.Add(measure1, "DataItem1")
+        Me.choroplethMapDashboardItem1.DataItemRepository.Add(measure2, "DataItem2")
+        Me.choroplethMapDashboardItem1.DataItemRepository.Add(measure3, "DataItem3")
+        Me.choroplethMapDashboardItem1.DataSource = Me.dashboardExcelDataSource1
+        Me.choroplethMapDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+        Me.choroplethMapDashboardItem1.InteractivityOptions.MasterFilterMode = DevExpress.DashboardCommon.DashboardItemMasterFilterMode.Multiple
+        valueMap1.Name = "Revenue YTD"
+        valueMap1.Scale = uniformScale1
+        valueMap1.ValueName = "Revenue"
+        valueMap1.AddDataItem("Value", measure1)
+        deltaMap1.AbsoluteVariationNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Number
+        deltaMap1.AbsoluteVariationNumericFormat.Precision = 0
+        deltaMap1.ActualValueName = "Revenue"
+        deltaMap1.DeltaName = "vs Target"
+        deltaMap1.Name = "Revenue YTD vs Target"
+        deltaMap1.PercentOfTargetNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Percent
+        deltaMap1.PercentVariationNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Percent
+        deltaMap1.AddDataItem("ActualValue", measure2)
+        deltaMap1.AddDataItem("TargetValue", measure3)
+        Me.choroplethMapDashboardItem1.Maps.AddRange(New DevExpress.DashboardCommon.ChoroplethMap() {valueMap1, deltaMap1})
+        Me.choroplethMapDashboardItem1.Name = "Sales by State"
+        Me.choroplethMapDashboardItem1.ShapeTitleAttributeName = "ABBREV"
+        Me.choroplethMapDashboardItem1.ShowCaption = True
+        Me.choroplethMapDashboardItem1.Viewport.BottomLatitude = 22.722765208057595R
+        Me.choroplethMapDashboardItem1.Viewport.CenterPointLatitude = 37.208247632673633R
+        Me.choroplethMapDashboardItem1.Viewport.CenterPointLongitude = -95.848499902568008R
+        Me.choroplethMapDashboardItem1.Viewport.LeftLongitude = -124.70997774915153R
+        Me.choroplethMapDashboardItem1.Viewport.RightLongitude = -66.987022055984482R
+        Me.choroplethMapDashboardItem1.Viewport.TopLatitude = 49.369672064487254R
+        '
+        ' gridDashboardItem1
+        '
+        dimension2.DataMember = "Product"
+        gridDimensionColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
+        gridDimensionColumn1.AddDataItem("Dimension", dimension2)
+        measure4.DataMember = "UnitsSoldYTD (Sum)"
+        measure5.DataMember = "UnitsSoldYTDTarget (Sum)"
+        gridDeltaColumn1.AbsoluteVariationNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Number
+        gridDeltaColumn1.AbsoluteVariationNumericFormat.Precision = 0
+        gridDeltaColumn1.Name = "UnitsSold YTD vs Target"
+        gridDeltaColumn1.PercentOfTargetNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Percent
+        gridDeltaColumn1.PercentVariationNumericFormat.FormatType = DevExpress.DashboardCommon.DataItemNumericFormatType.Percent
+        gridDeltaColumn1.WidthType = DevExpress.DashboardCommon.GridColumnFixedWidthType.Weight
+        gridDeltaColumn1.AddDataItem("ActualValue", measure4)
+        gridDeltaColumn1.AddDataItem("TargetValue", measure5)
+        Me.gridDashboardItem1.Columns.AddRange(New DevExpress.DashboardCommon.GridColumnBase() {gridDimensionColumn1, gridDeltaColumn1})
+        Me.gridDashboardItem1.ComponentName = "gridDashboardItem1"
+        Me.gridDashboardItem1.DataItemRepository.Clear()
+        Me.gridDashboardItem1.DataItemRepository.Add(dimension2, "DataItem0")
+        Me.gridDashboardItem1.DataItemRepository.Add(measure4, "DataItem1")
+        Me.gridDashboardItem1.DataItemRepository.Add(measure5, "DataItem2")
+        Me.gridDashboardItem1.DataSource = Me.dashboardExcelDataSource1
+        Me.gridDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+        Me.gridDashboardItem1.Name = "Product Units Sold"
+        Me.gridDashboardItem1.ShowCaption = True
+        '
+        ' dashboardExcelDataSource1
+        '
+        Me.dashboardExcelDataSource1.ComponentName = "dashboardExcelDataSource1"
+        Me.dashboardExcelDataSource1.FileName = "|DataDirectory|\Data\Sales.xlsx"
+        Me.dashboardExcelDataSource1.Name = "Excel Data Source 1"
+        Me.dashboardExcelDataSource1.ResultSchemaSerializable = resources.GetString("dashboardExcelDataSource1.ResultSchemaSerializable")
+        fieldInfo1.Name = "Category"
+        fieldInfo1.Type = GetType(String)
+        fieldInfo2.Name = "Product"
+        fieldInfo2.Type = GetType(String)
+        fieldInfo3.Name = "State"
+        fieldInfo3.Type = GetType(String)
+        fieldInfo4.Name = "UnitsSoldYTD (Sum)"
+        fieldInfo4.Type = GetType(Double)
+        fieldInfo5.Name = "UnitsSoldYTDTarget (Sum)"
+        fieldInfo5.Type = GetType(Double)
+        fieldInfo6.Name = "RevenueQTD (Sum)"
+        fieldInfo6.Type = GetType(Double)
+        fieldInfo7.Name = "RevenueQTDTarget (Sum)"
+        fieldInfo7.Type = GetType(Double)
+        fieldInfo8.Name = "RevenueYTD (Sum)"
+        fieldInfo8.Type = GetType(Double)
+        fieldInfo9.Name = "RevenueYTDTarget (Sum)"
+        fieldInfo9.Type = GetType(Double)
+        Me.dashboardExcelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9})
+        excelWorksheetSettings1.CellRange = Nothing
+        excelWorksheetSettings1.WorksheetName = "Sheet1"
+        excelSourceOptions1.ImportSettings = excelWorksheetSettings1
+        Me.dashboardExcelDataSource1.SourceOptions = excelSourceOptions1
+        '
+        ' SampleDashboard
+        '
+        Me.CurrencyCultureName = Nothing
+        Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() {Me.dashboardExcelDataSource1})
+        Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() {Me.choroplethMapDashboardItem1, Me.gridDashboardItem1})
+        dashboardLayoutItem1.DashboardItem = Me.choroplethMapDashboardItem1
+        dashboardLayoutItem1.Weight = 62.659846547314579R
+        dashboardLayoutItem2.DashboardItem = Me.gridDashboardItem1
+        dashboardLayoutItem2.Weight = 37.340153452685421R
+        dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutItem1, dashboardLayoutItem2})
+        dashboardLayoutGroup1.DashboardItem = Nothing
+        Me.LayoutRoot = dashboardLayoutGroup1
+        Me.Title.Text = "Sample Dashboard"
+        CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(measure2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(measure3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.choroplethMapDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(dimension2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(measure4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(measure5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gridDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+    End Sub
+
+    Friend WithEvents choroplethMapDashboardItem1 As DevExpress.DashboardCommon.ChoroplethMapDashboardItem
+    Friend WithEvents dashboardExcelDataSource1 As DevExpress.DashboardCommon.DashboardExcelDataSource
+    Friend WithEvents gridDashboardItem1 As DevExpress.DashboardCommon.GridDashboardItem
+
+End Class
